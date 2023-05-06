6285a394239c4494be04e881daff9f24fb8386f9
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-05-06 13:13:59 +0300

    Дополнен класс UserController

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,54 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
+@Slf4j
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserDto> getUsersList() {
+        log.info("Запрос на получение списка пользователей");
+        return userService.getUsersList();
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable("id") Integer userId) {
+        log.info("Запрос на получение данных пользователя с ID={}", userId);
+        return userService.getUser(userId);
+    }
+
+    @PostMapping
+    public UserDto createUser(@Valid @RequestBody UserDto userDto) {
+        log.info("Запрос на создание новой записи");
+        return userService.createUser(userDto);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto updateUser(@RequestBody UserDto userDto, @PathVariable("id") Integer userId) {
+        log.info("Запрос на обновление текущей записи");
+        return userService.updateUser(userDto, userId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Integer userId) {
+        log.info("Запрос на удаление записи с id = {}", userId);
+        userService.deleteUser(userId);
+    }
 }
