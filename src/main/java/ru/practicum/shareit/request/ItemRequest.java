0fac89dee712f788b382851268550bff19a8981f
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-05-06 13:20:08 +0300

    Дополнен класс ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequest {
+
+    private Integer id;
+
+    @Size(max = 200, message = "Максимальная длина строки с описанием — не более 200 символов")
+    private String description;
+
+    private Integer requestor;
+
+    private LocalDate created;
 }
+
