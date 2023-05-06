a8f1c544fcff2875939826a073113025240aa0c9
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-05-06 13:16:27 +0300

    Добавлен класс ItemMapper

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.item;
+
+public class ItemMapper {
+    public static ItemDto toDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .request(item.getRequest())
+                .build();
+    }
+    public static Item fromDto(ItemDto itemDto) {
+        return Item.builder()
+                .id(itemDto.getId())
+                .name(itemDto.getName())
+                .description(itemDto.getDescription())
+                .available(itemDto.getAvailable())
+                .request(itemDto.getRequest())
+                .build();
+    }
+}
