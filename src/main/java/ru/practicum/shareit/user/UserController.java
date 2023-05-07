64ebdd32b7ae555b4864183d090040eab5a231ba
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-05-07 20:46:59 +0300

    Внесены изменения для разделения методов валидации

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -2,9 +2,13 @@ package ru.practicum.shareit.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -32,13 +36,13 @@ public class UserController {
     }
 
     @PostMapping
-    public UserDto createUser(@Valid @RequestBody UserDto userDto) {
+    public UserDto createUser(@RequestBody @Validated(Create.class) UserDto userDto) {
         log.info("Запрос на создание новой записи");
         return userService.createUser(userDto);
     }
 
     @PatchMapping("/{id}")
-    public UserDto updateUser(@RequestBody UserDto userDto, @PathVariable("id") Long userId) {
+    public UserDto updateUser(@RequestBody @Validated(Update.class) UserDto userDto, @PathVariable("id") Long userId) {
         log.info("Запрос на обновление текущей записи");
         return userService.updateUser(userDto, userId);
     }
@@ -47,5 +51,17 @@ public class UserController {
     public void deleteUser(@PathVariable("id") Long userId) {
         log.info("Запрос на удаление записи с id = {}", userId);
         userService.deleteUser(userId);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errorMap = new ArrayList<>();
+
+        for (FieldError err : e.getBindingResult().getFieldErrors()) {
+            errorMap.add(err.getDefaultMessage());
+        }
+
+        return errorMap;
     }
 }
