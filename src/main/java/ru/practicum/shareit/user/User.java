9493c4f9acaf7634085b4486ce539c457c0ab53f
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-07-01 02:54:57 +0300

    Внесены изменения

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -5,12 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="users", schema="public")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_Id")
     private Long id;
+
+    @Column(name="name", nullable = false)
     private String name;
+
+    @Column(name="email", nullable = false)
     private String email;
 }
