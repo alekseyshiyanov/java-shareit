ca965a1f1040bedb84fcd5eb23c9938298b54bad
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-07-01 02:55:46 +0300

    Добавлен UserRepository

---
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByEmailContainingIgnoreCase(String emailSearch);
+
+    @Transactional
+    void deleteUserById(Long UserId);
+
+    User getUserById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query("update User u " +
+            "set u.name  = COALESCE(CAST(:#{#newUser.name}  as string), u.name ), " +
+                "u.email = COALESCE(CAST(:#{#newUser.email} as string), u.email)  " +
+            "where  u.id = :#{#newUser.id}")
+    void updateUser(@Param("newUser") User newUser);
+}
