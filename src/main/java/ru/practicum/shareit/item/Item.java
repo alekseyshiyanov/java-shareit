eca8cf8c55fb55f77aaadfcf41fae2266691106f
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-07-13 16:19:55 +0300

    Внесены изменения для работы с hibernate search

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -1,26 +1,46 @@
 package ru.practicum.shareit.item;
 
 import lombok.*;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.StopFilterFactory;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+import org.hibernate.search.annotations.*;
+import org.hibernate.search.annotations.Parameter;
 import ru.practicum.shareit.user.User;
 
 import javax.persistence.*;
 
 @Entity
+@Indexed(index = "idx_item")
 @Table(name = "item", schema = "public")
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@AnalyzerDef(name = "textAnalyzer",
+        tokenizer = @TokenizerDef(factory = StandardTokenizerFactory.class),
+        filters = { @TokenFilterDef(factory = LowerCaseFilterFactory.class),
+                    @TokenFilterDef(
+                        factory = StopFilterFactory.class,
+                        params = {
+                                @Parameter(name = "words", value = "stopwords_ru.txt"),
+                                @Parameter(name = "ignoreCase", value = "true")
+                        }
+                )
+        })
+@Analyzer(definition = "textAnalyzer")
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_id")
     private Long id;
 
+    @Field(name="nameFiltered", termVector = TermVector.YES)
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Field(name="descriptionFiltered", termVector = TermVector.YES)
     @Column(name = "description", nullable = false)
     private String description;
 
