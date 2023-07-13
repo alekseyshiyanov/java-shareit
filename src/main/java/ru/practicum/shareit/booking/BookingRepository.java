4035753a942b2c69a20f6737852f43df5b9bc9a4
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-07-13 16:27:20 +0300

    Внесены изменения

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -1,12 +1,13 @@
 package ru.practicum.shareit.booking;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
@@ -21,57 +22,72 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
             "where b.id = :bookingId")
     void updateStatus(@Param("bookingId") Long bookingId, @Param("status") BookingStatus status);
 
-    List<Booking> getAllBookingByBooker_IdOrderByStartDesc(Long userId);
+    Page<Booking> getAllBookingByBooker_IdOrderByStartDesc(Long userId, Pageable pageable);
 
-    List<Booking> getAllBookingByBooker_IdAndStatusOrderByStartDesc(Long userId, BookingStatus status);
+    Page<Booking> getAllBookingByBooker_IdAndStatusOrderByStartDesc(Long userId, BookingStatus status, Pageable pageable);
 
     @Query("select b " +
             "from Booking b " +
             "where b.booker.id = :bookerId " +
             "and b.start > :current " +
             "order by b.start desc")
-    List<Booking> getAllBookingByUserInFuture(@Param("bookerId") Long bookerId, @Param("current") LocalDateTime current);
+    Page<Booking> getAllBookingByUserInFuture(@Param("bookerId") Long bookerId,
+                                              @Param("current") LocalDateTime current,
+                                              Pageable pageable);
 
-    List<Booking> getAllByBooker_IdAndEndIsAfterAndStartBeforeOrderByStartDesc(Long bookerId, LocalDateTime current1, LocalDateTime current2);
+    Page<Booking> getAllByBooker_IdAndEndIsAfterAndStartBeforeOrderByStartDesc(Long bookerId,
+                                                                               LocalDateTime current1,
+                                                                               LocalDateTime current2,
+                                                                               Pageable pageable);
 
     @Query("select b " +
             "from Booking b " +
             "where b.booker.id = :bookerId " +
             "and b.end < :current " +
             "order by b.start desc")
-    List<Booking> getAllBookingByUserInPast(@Param("bookerId") Long bookerId, @Param("current") LocalDateTime current);
+    Page<Booking> getAllBookingByUserInPast(@Param("bookerId") Long bookerId,
+                                            @Param("current") LocalDateTime current,
+                                            Pageable pageable);
 
     @Query("select b " +
             "from Booking b " +
             "where b.item.user.id = :ownerId " +
             "order by b.start desc")
-    List<Booking> getAllBookingByOwner(@Param("ownerId") Long ownerId);
+    Page<Booking> getAllBookingByOwner(@Param("ownerId") Long ownerId, Pageable pageable);
 
     @Query("select b " +
             "from Booking b " +
             "where b.item.user.id = :ownerId " +
             "and b.start > :current " +
             "order by b.start desc")
-    List<Booking> getAllBookingByOwnerInFuture(@Param("ownerId") Long ownerId, @Param("current") LocalDateTime current);
+    Page<Booking> getAllBookingByOwnerInFuture(@Param("ownerId") Long ownerId,
+                                               @Param("current") LocalDateTime current,
+                                               Pageable pageable);
 
     @Query("select b " +
             "from Booking b " +
             "where b.item.user.id = :ownerId " +
             "and :current between b.start and b.end " +
             "order by b.start desc")
-    List<Booking> getAllBookingByOwnerInCurrent(@Param("ownerId") Long ownerId, @Param("current") LocalDateTime current);
+    Page<Booking> getAllBookingByOwnerInCurrent(@Param("ownerId") Long ownerId,
+                                                @Param("current") LocalDateTime current,
+                                                Pageable pageable);
 
     @Query("select b " +
             "from Booking b " +
             "where b.item.user.id = :ownerId " +
             "and b.end < :current " +
             "order by b.start desc")
-    List<Booking> getAllBookingByOwnerInPast(@Param("ownerId") Long ownerId, @Param("current") LocalDateTime current);
+    Page<Booking> getAllBookingByOwnerInPast(@Param("ownerId") Long ownerId,
+                                             @Param("current") LocalDateTime current,
+                                             Pageable pageable);
 
     @Query("select b " +
             "from Booking b " +
             "where b.item.user.id = :ownerId " +
             "and b.status = :status " +
             "order by b.start desc")
-    List<Booking> getAllBookingByOwnerAndStatus(@Param("ownerId") Long ownerId, @Param("status") BookingStatus status);
+    Page<Booking> getAllBookingByOwnerAndStatus(@Param("ownerId") Long ownerId,
+                                                @Param("status") BookingStatus status,
+                                                Pageable pageable);
 }
