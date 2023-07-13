dacf7bb7a4c312bdaf0032d03d991cf35c85150b
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-07-13 16:26:24 +0300

    Внесены изменения

--- src/main/java/ru/practicum/shareit/request/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestDto.java
@@ -1,7 +1,29 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.ItemDto;
+import ru.practicum.shareit.user.UserDto;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequestDto {
+    private Long id;
+
+    @NotBlank(message = "Описание не должно быть пустым или равно null")
+    private String description;
+
+    private UserDto requester;
+
+    private List<ItemDto> items;
+
+    private LocalDateTime created;
 }
