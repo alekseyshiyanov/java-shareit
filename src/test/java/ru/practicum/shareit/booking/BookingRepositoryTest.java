5eb8c4a04cb9a50d43f9a550ddb2bcc3ebd93556
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-07-14 17:43:52 +0300

    Внесены изменения в BookingRepository

--- src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -8,9 +8,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.util.Assert;
 import ru.practicum.shareit.item.Item;
 import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.user.User;
