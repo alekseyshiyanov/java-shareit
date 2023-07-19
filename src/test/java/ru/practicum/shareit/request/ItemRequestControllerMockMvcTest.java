d1dbd8e41635f0b00bd194025c88610febebf4d3
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-07-19 13:25:19 +0300

    Внесены изменения

--- src/test/java/ru/practicum/shareit/request/ItemRequestControllerMockMvcTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestControllerMockMvcTest.java
@@ -1,16 +1,12 @@
 package ru.practicum.shareit.request;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
